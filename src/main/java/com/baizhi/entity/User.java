0c3cd2006c4f64aed495d3c2c0bead0f0c612b60
zhengjc <zhengjc0621@163.com>
2018-06-08 17:05:02 +0800

    第五次提交

--- src/main/java/com/baizhi/entity/User.java
+++ src/main/java/com/baizhi/entity/User.java
@@ -5,6 +5,15 @@ package com.baizhi.entity;
  */
 public class User {
     private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private String name;
     private String password;
 
